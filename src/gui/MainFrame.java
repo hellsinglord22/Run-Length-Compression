cd466cecb1d9db08617134b4b853493c57a7bd0c
H <heshamscorpe55@gmail.com>
2015-05-13 00:23:27 +0200

    Set components properties

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -4,6 +4,37 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
-    
+    /// Attributes ///
+    private Container mainFrameContainer;
+    private ComparisonPanel mainFrameComparisonPanel;
+
+
+    /// Constructor ///
+    public MainFrame(String title) {
+            super(title);
+
+        /// Initialize attributes ///
+        mainFrameContainer = getContentPane();
+        mainFrameComparisonPanel = new ComparisonPanel();
+
+        // set properties ///
+        setLayout(new BorderLayout());
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setVisible(true);
+        setComponentsPositions();
+        setComponentsBehaviour();
+    }
+
+    /// Methods ///
+    private void setComponentsPositions() {
+
+    }
+
+    private void setComponentsBehaviour() {
+
+    }
+
 
 }

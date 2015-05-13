5c58f43daf4d906d5029846ae961ca55025a9584
H <heshamscorpe55@gmail.com>
2015-05-13 19:17:34 +0200

    Create 'CompressionInfo' event

---
+++ src/gui/CompressionInfo.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.util.EventObject;
+
+public class CompressionInfo extends EventObject {
+
+    /// Attributes ///
+    private String inputPath;
+    private Order buttonClicked;
+
+
+    public CompressionInfo(Object source , String inputPath , Order buttonClicked) {
+        super(source);
+        this.inputPath = inputPath;
+        this.buttonClicked = buttonClicked;
+    }
+
+    /// getters and setters ///
+    public Order getButtonClicked() {
+        return buttonClicked;
+    }
+
+    public void setButtonClicked(Order buttonClicked) {
+        this.buttonClicked = buttonClicked;
+    }
+
+    public String getInputPath() {
+        return inputPath;
+    }
+
+    public void setInputPath(String inputPath) {
+        this.inputPath = inputPath;
+    }
+}

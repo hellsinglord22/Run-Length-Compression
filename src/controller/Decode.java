98865bfb89ae5cd62ffb5d8ea6fe9d9fa028f7de
H <heshamscorpe55@gmail.com>
2015-05-14 22:12:47 +0200

    Add Encode and the Decode

---
+++ src/controller/Decode.java
@@ -0,0 +1,45 @@
+package controller;
+
+import gui.CompressionInfo;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Decode implements CompressionBehaviour {
+    @Override
+    public void encodeOrDecode(CompressionInfo event) {
+        try {
+            String path = event.getInputPath();
+            File inputFile = new File(path);
+            File outputFile = new File("output.txt");
+            String data = FileUtils.readFileToString(inputFile);
+            data = decode(data);
+            System.out.println(data);
+            FileUtils.write(outputFile, data);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File was not found!!!");
+        } catch (IOException e) {
+            System.err.println("You don't have input/output permission!!");
+        }
+
+
+    }
+    private static String decode(String source) {
+        StringBuffer dest = new StringBuffer();
+        Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z]");
+        Matcher matcher = pattern.matcher(source);
+        while (matcher.find()) {
+            int number = Integer.parseInt(matcher.group());
+            matcher.find();
+            while (number-- != 0) {
+                dest.append(matcher.group());
+            }
+        }
+        return dest.toString();
+    }
+}
---
+++ src/controller/Encode.java
@@ -0,0 +1,42 @@
+package controller;
+
+import gui.CompressionInfo;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Encode implements CompressionBehaviour {
+    @Override
+    public void encodeOrDecode(CompressionInfo event) {
+        try {
+            String path = event.getInputPath();
+            File inputFile = new File(path);
+            File outputFile = new File("output.txt");
+            String data = FileUtils.readFileToString(inputFile);
+            data = encode(data);
+            System.out.println(data);
+            FileUtils.write(outputFile, data);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File was not found!!!");
+        } catch (IOException e) {
+            System.err.println("You don't have input/output permission!!");
+        }
+
+    }
+    private static String encode(String source) {
+        StringBuffer dest = new StringBuffer();
+        for (int i = 0; i < source.length(); i++) {
+            int runLength = 1;
+            while (i+1 < source.length() && source.charAt(i) == source.charAt(i+1)) {
+                runLength++;
+                i++;
+            }
+            dest.append(runLength);
+            dest.append(source.charAt(i));
+        }
+        return dest.toString();
+    }
+}
